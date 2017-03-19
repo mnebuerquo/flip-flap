6ee1ba0828d5cba836d7bed72fd26a992dba9809
Jason Mize <jasonemize@gmail.com>
2017-03-18 19:12:54 -0500

    basic seed for webpack, npm, react, redux

---
+++ apps/static/js/index.js
@@ -0,0 +1,27 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// // import { Provider } from 'react-redux';
+// // import { Router, Route, hashHistory, Redirect } from 'react-router';
+// // import { syncHistoryWithStore } from 'react-router-redux';
+
+// // import dashboardStore from './stores/dashboardStore';
+
+// // import DashboardPage from './components/DashboardPage';
+
+// // global.reduxStore = dashboardStore;
+// // global.BASE_STATIC_URL = window.location.host === 'educator-certification.ogapcusa.org' ?
+//     // 'https://s3.amazonaws.com/educator-certification.ogapcusa.org' :
+//     // '/static';
+
+// // const history = syncHistoryWithStore(hashHistory, dashboardStore);
+
+// ReactDOM.render(
+//     // <Provider store={dashboardStore}>
+//         // <Router history={history}>
+//         //     <Redirect from="/" to="dashboard/" />
+//         //     <Route path="dashboard/" component={DashboardPage} />
+//         // </Router>
+//     // </Provider>,
+//     document.getElementById('root'),
+// );
+
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require('path')
+var webpack = require ('webpack')
+var BundleTracker = require('webpack-bundle-tracker')
+
+module.exports = {
+    context: __dirname,
+    entry: [
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/only-dev-server',
+        './apps/static/js/index'
+    ],
+    output: {
+        path: path.resolve('./apps/static/bundles/'),
+        filename: 'bundle.js',
+        publicPath: 'http://localhost:3000/static/bundles', // Tell Django to use this URL to load packages and not use STATIC_URL + bundle_name
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),  // don't reload if there is an error
+        new BundleTracker({filename: './webpack-stats.json'}),
+    ],
+    module: {
+        loaders: [
+            // we pass the output from babel loader to react-hot loader
+            { test: /\.js?$/, exclude: [/node_modules/], loaders: ['babel-loader'], },
+            { test: /\.html$/, loader: 'raw' }, 
+        ],
+    },
+    resolve: {
+        // modulesDirectories: ['node_modules', 'bower_components'],
+        extensions: ['.js', '.jsx']
+    }
+}
+
